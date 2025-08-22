3af0a1c4ebb4db8b5040ca26c372e9e1928abdb5
Nikita <nyuskov@gmail.com>
2025-08-22 17:45:28 +0300

    new code for blog

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def home():
-    return {"message": "Hello World!"}
+@app.get("/blog")
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published blogs from db"}
+    else:
+        return {"data": f"{limit} blogs from db"}
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "al unpublished blogs"}
+
+
+@app.get("/blog/{blog_id}")
+def show(blog_id: int):
+    # fetch blog with id = blog_id
+    return {"data": blog_id}
+
+
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id: int, limit: int = 10):
+    # fetch comments of blog with id = blog_id
+
+    return {"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {"data": f"Blog is created with title as {blog.title}"}
